46fb52ebbf101be27c4249256f5d2dfe4b2a7a18
zunamitd <zunamihd@outlook.de>
2026-06-10 10:49:24 +0200

    aaaa

--- target/linux/qualcommax/image/ipq50xx.mk
+++ target/linux/qualcommax/image/ipq50xx.mk
@@ -6,16 +6,9 @@ define Device/jdcloud_be6500
   DEVICE_DTS_DIR := $(TOPDIR)/target/linux/qualcommax/dts
   DEVICE_DTS_CONFIG := config@mi01.6
   SUPPORTED_DEVICES := jdcloud,be6500
-  # ---------- FIX UBI GEOMETRY ----------
-  BLOCKSIZE := 0x20000
-  PAGESIZE  := 0x800
-  SUBPAGESIZE := 0x800
-
-  KERNEL_IN_UBI := 1
-
-  IMAGES += factory.ubi
-
-  IMAGE/factory.ubi := append-ubi | check-size $$$$(KERNEL_SIZE)
   DEVICE_PACKAGES := kmod-ath12k kmod-qca-nss-dp luci luci-ssl apk-mbedtls block-mount uboot-envtools e2fsprogs kmod-fs-ext4
+  # FORCE FACTORY IMAGE
+  IMAGES += factory.bin
+  IMAGE/factory.bin := append-rootfs | pad-rootfs | pad-to 64k
 endef
 TARGET_DEVICES += jdcloud_be6500
